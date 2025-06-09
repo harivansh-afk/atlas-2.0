b97ab6cf58777ee48d7ba8c31099f64a122c71b3
Harivansh Rathi <rathiharivansh@gmail.com>
2025-06-09 09:46:38 -0400

    chore: lead management script

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -123,6 +123,12 @@ export default function RootLayout({
           })(window,document,'script','dataLayer','GTM-PCHSN4M2');`}
         </Script>
         {/* End Google Tag Manager */}
+
+        {/* REB2B Lead Tracking */}
+        <Script id="reb2b-tracking" strategy="afterInteractive">
+          {`!function () {var reb2b = window.reb2b = window.reb2b || [];if (reb2b.invoked) return;reb2b.invoked = true;reb2b.methods = ["identify", "collect"];reb2b.factory = function (method) {return function () {var args = Array.prototype.slice.call(arguments);args.unshift(method);reb2b.push(args);return reb2b;};};for (var i = 0; i < reb2b.methods.length; i++) {var key = reb2b.methods[i];reb2b[key] = reb2b.factory(key);}reb2b.load = function (key) {var script = document.createElement("script");script.type = "text/javascript";script.async = true;script.src = "https://s3-us-west-2.amazonaws.com/b2bjsstore/b/" + key + "/0OV0VHMR9Y6Z.js.gz";var first = document.getElementsByTagName("script")[0];first.parentNode.insertBefore(script, first);};reb2b.SNIPPET_VERSION = "1.0.1";reb2b.load("0OV0VHMR9Y6Z");}();`}
+        </Script>
+        {/* End REB2B Lead Tracking */}
       </head>
 
       <body
